c5b6e9885afe786eb8194ef79e883e6899b0806e
seungyeah <closer.fz@gmail.com>
2025-10-19 21:19:40 +0900

    feat(ApplicationTest): 문자열 계산기 테스트 추가
    구분자를 등록하지 않은 경우와 구분자가 한글자 이상인 경우에 대한 테스트 케이스를 추가함. 이를 통해 문자열 계산기의 입력값 검증 및 예외 처리를 강화함.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,12 +32,36 @@ describe("문자열 계산기", () => {
     });
   });
 
+
+  test("구분자를 등록하지 않은 경우", async () => {
+    const inputs = ["//\\n123"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 123"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("예외 테스트", async () => {
     const inputs = ["-1,2,3"];
     mockQuestions(inputs);
 
     const app = new App();
 
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("예외 테스트: 구분자가 한글자 이상이 경우", async () => {
+    const inputs = ["//;;\\n1;;2;3"];
+    mockQuestions(inputs);
+
+    const app = new App();
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
 });
